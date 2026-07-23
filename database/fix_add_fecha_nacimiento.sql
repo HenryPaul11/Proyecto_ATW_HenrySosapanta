7a461cddd8cec607bd3c9d4d6b744a2dfb378e7a
HenryPaul11 <henrypauls705@gmail.com>
2026-07-22 21:47:38 -0500

    depuracion

--- database/fix_add_fecha_nacimiento.sql
+++
@@ -1,3 +0,0 @@
--- Migracion: agregar columna fecha_nacimiento a la tabla clientes
--- Ejecutar si la columna no existe en la BD
-ALTER TABLE clientes ADD COLUMN IF NOT EXISTS fecha_nacimiento DATE;
